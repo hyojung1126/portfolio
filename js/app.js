db33d9cf83c5eaf8772ef437e98409b2fd023e3f
hyojung1126 <chlgywjd50@naver.com>
2023-08-16 09:23:02 +0900

    first commit

--- js/app.js
+++ js/app.js
@@ -3,28 +3,5 @@ $(function () {
     $('.header').load('../inc/header.html')
 
 // 포트폴리오 페이지
-$('.tab_menu p').click(function(){
-    $('.tab_menu p').removeClass('active')
-    $(this).addClass('active')
-})
-
-$('.tab_menu p').eq(0).click(function(){
-    $('.portfolio_contents .contents').show()
-})
-
-$('.tab_menu p').eq(1).click(function(){
-    $('.portfolio_contents .contents').hide()
-    $('.portfolio_contents .contents.web').show()
-})
-
-$('.tab_menu p').eq(2).click(function(){
-    $('.portfolio_contents .contents').hide()
-    $('.portfolio_contents .contents.m').show()
-})
-
-$('.tab_menu p').eq(3).click(function(){
-    $('.portfolio_contents .contents').hide()
-    $('.portfolio_contents .contents.UI').show()
-})
     
 })
